4b68cd9b695b248efe22c095280ea65452ce3253
zeroprin-zf <zerozerozf07@outlook.jp>
2024-04-03 10:54:56 +0900

    メソッド学習

---
+++ src/method/java/method1.java
@@ -0,0 +1,17 @@
+package method.java;
+
+public class method1 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		System.out.println("メソッドを呼び出します");
+		hello();
+		System.out.println("メソッドの呼び出しが終わりました。");
+	}
+	
+	public static void hello() { //メソッドの定義
+		System.out.println("湊さん、こんにちは"); //helloメソッドが呼び出されたときの定義
+	}//↑メソッドブロック
+
+}
---
+++ src/method/java/method2.java
@@ -0,0 +1,20 @@
+package method.java;
+
+public class method2 {
+	
+	public static void methodA() {
+		System.out.println("methodA");
+		methodB();
+	}
+	
+	public static void methodB() {
+		System.out.println("methodB");
+	}
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		methodA();
+	}
+
+}
