e4904f99784f69813e4cda1542f8fde3e6fbcc46
zeroprin-zf <zerozerozf07@outlook.jp>
2024-04-06 09:38:20 +0900

    複数の引数

---
+++ src/method/java/argument2.java
@@ -0,0 +1,17 @@
+package method.java;
+
+public class argument2 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		add(100, 20);
+		add(200, 50);
+	}
+	
+	public static void add(int x, int y) {
+		int ans = x + y;
+		System.out.println(x + "+" + y + "=" + ans);
+	}
+
+}
