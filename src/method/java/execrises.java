ece54f041b2398553bb530b2927361e759ef76ab
zeroprin-zf <zerozerozf07@outlook.jp>
2024-04-09 10:18:42 +0900

    練習問題1

---
+++ src/method/java/execrises.java
@@ -0,0 +1,21 @@
+package method.java;
+
+public class execrises {
+
+	public static void introduceOneself(){
+		String name = "ユウダイ";
+		int age = 28;
+		double height = 133.3;
+		char zodiac = '虎';
+		System.out.println("私の名前は" + name + "です");
+		System.out.println("歳は" + age + "です");
+		System.out.println("身長は" + height + "cmです");
+		System.out.println("十二支は" + zodiac + "です");
+	}
+	
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		introduceOneself();
+	}
+
+}
