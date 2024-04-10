9453296d83dba1ced4444d2ec385e3500f383573
zeroprin-zf <zerozerozf07@outlook.jp>
2024-04-10 20:34:55 +0900

    練習問題3

---
+++ src/method/java/execrises3.java
@@ -0,0 +1,24 @@
+package method.java;
+
+public class execrises3 {
+	
+		public static void email(String address, String text) {
+			System.out.println(address + "に、以下のメールを送信しました");
+			System.out.println("件名：無題");
+			System.out.println("本文：" + text);
+		}
+		
+		public static void main(String[] args) {
+			// TODO 自動生成されたメソッド・スタブ
+			String address = "samoedo@mail";
+			String text = "そういうこと";
+			email(address, text);
+		}
+		
+		public static void email (String title, String address, String text) {
+			System.out.println(address + "に、以下のメールを転送しました。");
+			System.out.println("件名:" + title);
+			System.out.println("本文：" + text);
+	}
+
+}
